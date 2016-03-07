2bb9b4cf5314f7513044a1c200c0e7daf5ca1328
Lei <dinglei197@gmail.com>
2016-03-07 14:30:46 +0800

    update question module

--- src/module/pageCtrl/question.edit.js
+++ src/module/pageCtrl/question.edit.js
@@ -2,6 +2,7 @@
 
 export default class QuesEditCtrl {
   constructor($scope, $location, Util, $stateParams, Cfg, Rest) {
+    var oldData;
     if($stateParams.id * 1 != $stateParams.id){
         $location.path('/question');
         return
@@ -42,17 +43,49 @@ export default class QuesEditCtrl {
       $scope.title = '';
       $scope.description = '';
       $scope.question = [];
+    },
+    submit = () => {
+      let sender = {};
+      let data = {
+          title: $scope.title.trim(),
+          description: $scope.description.trim(),
+          question: $scope.question
+      };
+      if(data.title.length < 1 || data.question.length < 1){
+          alert('标题和问题必填！');
+          return
+      }
+      if(oldData.title != data.title){
+          sender.title = data.title
+      }
+      if(oldData.description != data.description){
+          sender.description = data.description
+      }
+
+      if(JSON.stringify(oldData.question) != JSON.stringify(data.question)){
+          sender.question = data.question
+      }
+
+      let Question = Rest.Question();
+      Question.update({sid: $scope.sid}, sender).$promise.then(() => {
+          alert('修改成功！');
+          $location.path('/questions')
+      }, () => { alert('修改失败！')})
     }
     $scope.$on('removeData', (e, d) => {
       $scope.question.splice(d, 1);
     });
 
+
+
     let Question = Rest.Question();
     Question.get({sid: $stateParams.id*1}).$promise.then((d) => {
+        oldData = angular.copy(d.data);
         Object.assign($scope, d.data);
         $scope.addItem = addItem;
         $scope.preview = preview;
         $scope.reset = reset;
+        $scope.submit = submit;
     }, () => { alert('获取数据失败！'); })
   }
 }
