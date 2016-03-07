69beb8bc442ac2a5fab221d9f8857f45c215b0ea
Lei <dinglei197@gmail.com>
2016-03-07 17:09:27 +0800

    update task module

--- src/module/pageCtrl/task.add.js
+++ src/module/pageCtrl/task.add.js
@@ -1,48 +1,52 @@
 'use strict';
 
 export default class TasksAddCtrl {
-  constructor($scope, Util, Cfg, uibDateParser) {
-      $scope.title = 'abc';
-      $scope.questions = [
-        {
-          id:0,
-          title: '第一个问卷'
-        },
-        {
-          id:1,
-          title: '第二个问卷'
-        }
-      ];
-      $scope.users = [
-        {
-          checked: false,
-          name: 'Lei',
-          email: 'leidin@microsoft.com',
-          gender: 'male',
-          age: '28',
-          job: 'Dev',
-          mobileos: 'IOS',
-          mobilebd: 'Apple',
-          pcos: 'Window 8',
-          state: '1',
-          location: 'Bei Jing'
-        }
-      ]
-      $scope.quest = $scope.questions[0];
+  constructor($scope, $q, Rest, Util, Cfg, uibDateParser) {
+      $scope.title = '';
+      $scope.questions = [];
+      $scope.users = [];
+      let User = Rest.User(), Question = Rest.Question();
+      $q.all([
+          User.query({}).$promise,
+          Question.query({}).$promise
+      ]).then((response) => {
+          $scope.users = response[0].data;
+          $scope.questions = response[1].data;
+          $scope.quest = $scope.questions[0];
+          $scope.submit = submit;
+      }, () => { alert('request error') })
+
       $scope.selected = (index) => {
           $scope.users[index].checked = !$scope.users[index].checked;
       }
 
       $scope.d = {
-        duration: new Date(),
+        date: new Date(),
         minDate: new Date(),
         altFormats: ['M!/d!/yyyy'],
-        time: new Date(),
-        ismeridian: true,
         open(){
           $scope.d.isOpen = true;
         }
       }
-
+      $scope.t1 = {
+         time: new Date(),
+         ismeridian: false
+      }
+      $scope.t2 = {
+         time: new Date(),
+         ismeridian: false,
+         day: 1
+      }
+      $scope.b = {
+        date: new Date(),
+        minDate: new Date(),
+        altFormats: ['M!/d!/yyyy'],
+        open(){
+          $scope.b.isOpen = true;
+        }
+      }
+      var submit = () => {
+          
+      }
   }
 }
