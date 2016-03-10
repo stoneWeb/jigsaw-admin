ced8c12f16e3d3ea230b955eb4e77e0fa72dcb00
Lei <dinglei197@gmail.com>
2016-03-10 12:14:01 +0800

    login ctrl

--- src/module/pageCtrl/login.js
+++ src/module/pageCtrl/login.js
@@ -1,7 +1,7 @@
 'use strict';
 export default class LoginCtrl {
-  constructor($scope, $rootScope, $location, Storage, Rest) {
-      if($rootScope.user){
+  constructor($scope, $location, Storage, Rest) {
+      if(Rest.user){
           return $location.path('/dashboard')
       }
       $scope.submit = () => {
@@ -11,12 +11,18 @@ export default class LoginCtrl {
           Rest.getToken($scope.email.trim(), $scope.password)
             .$promise.then(
               (d) => {
-                  $rootScope.user = d.user;
+                  d = d.data;
+                  if(d.user.uid != 1){
+                      alert('您不是管理员！');
+                      return;
+                  }
+                  Rest.user = d.user;
                   Storage.set('token', d.token);
                   setTimeout(Rest.refreshToken, d.duration*1000 - 10000);
                   $location.path('/dashboard');
               },
               () => {
+                  alert('登陆失败！');
                   console.log('login error');
               }
             )
--- src/module/pageCtrl/menu.js
+++ src/module/pageCtrl/menu.js
@@ -1,6 +1,13 @@
 'use strict';
 export default class MenuCtrl {
-  constructor() {
-    //console.log('menu');
+  constructor($scope, $location, Storage, Rest) {
+    if(!Storage.get('token')){
+        return $location.path('/login')
+    }
+    $scope.logout = () => {
+        Storage.remove('token');
+        Rest.user = null;
+        $location.path('/login');
+    }
   }
 }
